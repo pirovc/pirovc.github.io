137bbde908e3b02d9cbb8cc2297192e5487f500d
pirovc <pirovc@posteo.net>
2025-11-21 17:21:44 +0100

    bib

--- trim_bib.sh
+++
@@ -1,4 +0,0 @@
-sed -i 's/file =.*//g' pirovc.bib
-sed -i 's/keywords =.*//g' pirovc.bib
-sed -i 's/urldate =.*//g' pirovc.bib
-#awk '{if ($0 ~ /date =/) {print "\tyear = {"substr($0,10,4) "},"}else{print}}' pirovc.bib > tmp && mv tmp pirovc.bib
